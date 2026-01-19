54d6479ad33f46e7bdadc838857c9fdc6de5b303
Tareq M. Alkraimy — AI & Digital Transformation Engineer <sumerianur5@gmail.com>
2026-01-19 09:32:31 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,6 @@ from googleapiclient.errors import HttpError
 
 # ================= App setup =================
 st.set_page_config(page_title="IMS — Thi Qar Oil Company", layout="wide")
-st.sidebar.write(list(st.secrets.keys()))
 # ================= Styling ===================
 st.markdown(
     """
@@ -572,4 +571,5 @@ st.markdown(
     "<div class='sig'>تصميم وتطوير رئيس مهندسين أقدم طارق مجيد الكريمي ©</div>",
     unsafe_allow_html=True,
 )
+
 
